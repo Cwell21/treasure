5af0718743a9b11e494522395acb32c7f573d274
Cwell21 <cwe11@yahoo.com>
2019-06-08 13:00:49 -0600

    Logic of game seems to be working.  needs to be fully tested.  Next step to code win / lose sequence

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 
-const elBoard = document.getElementById("gameBoard");
-const elControl = document.getElementById("sideControls");
+//STORE BOARD AND CONTROL ELEMENTS FROM HTML DOCUMENT
+const elBoard = document.getElementById('gameBoard');
+const elControl = document.getElementById('sideControls');
+
+
+//CODE FOR INITIAL PLACEMENT OF HERO (TILE 0)
+let elCurLoc = [0,0];
 
 //FUNCTION TO CHECK HERO LOCATION VS GAME NODE LOCATIONS
 let chkLocation = heroLoc => {
-  if(virBoard[heroLoc] != 'x') {
-    if(virBoard[heroLoc] === 't') {
+  if(virBoard[elCurLoc[0]][elCurLoc[1]] != 'x') {
+    if(virBoard[elCurLoc[0]][elCurLoc[1]] === 't') {
       console.log('win');
       return 'win';
     } else {
@@ -18,38 +23,32 @@ let chkLocation = heroLoc => {
 
 //GAME OPTIONS
 let gameOptions = {
-  boardSize: 36,
-  tileId: 0,
+  boardSize: 36, //NUMBER OF TOTAL TILES (COL = SQRT ROW = SQRT)
+  tileId: 0, //PROPERTY TO KEEP TRACK OF ID FOR EACH TILE
   dragon: {
-    name: "Lisa",
-    image: "./images/Dragon1.jpg",
+    name: 'Lisa',
+    image: './images/Dragon1.jpg',
     location: [
-      0, //X
-      0, //Y
+      0, //Y - ROW
+      0, //X - CELL
     ]
   },
   treasure: {
-    image: "./images/treasure.jpg",
+    image: './images/treasure.jpg',
     location: [
-      0, //X
-      0, //Y
+      0, //Y - ROW
+      0, //X - CELL
     ]
   },
   user: {
-    name: "Brave Warrior",
-    image: "./images/hero.png",
+    name: 'Brave Warrior',
+    image: './images/hero.png',
     location: [
-      0, //X
-      0, //Y
+      0, //Y - ROW
+      0, //X - CELL
     ],
   },
 };
-
-function moveChar(loc, cell ,row) {
-
-
-
-}
 
 /***************************************
 GAME BOARD OBJECT
@@ -60,32 +59,70 @@ METHODS AND PROPERTIES:
 ***************************************/
 
 let gameBoard = {
-  createBoard: (tileId) => {
-    let elDiv = document.createElement("div");
-    elDiv.setAttribute("id", tileId);
-    elDiv.setAttribute("class", "brdTile");
-    elBoard.appendChild(elDiv);
-    gameOptions.tileId += 1;
+  createBoard: (row) => {
+    let elDivRow = document.createElement('div');
+
+    elDivRow.setAttribute('class', 'row');
+    elBoard.appendChild(elDivRow);
+
+    for (let i=0; i < Math.sqrt(gameOptions.boardSize); i++) {
+      let elDiv = document.createElement('div');
+      let id = row.toString() + i.toString();
+
+      elDiv.setAttribute('class', 'x');
+      elDiv.setAttribute('id', id);
+      elDivRow.appendChild(elDiv);
+    }
+
+    document.getElementById('00').classList.add('hero');
+
   },
 
   fillBoard: () => {
-
     // RANDOMLY PLACE TREASURE NODE (LOCATION 0 NOT ALLOWED)
     do {
-      gameOptions.treasure.location = Math.round(Math.floor(Math.random() * gameOptions.boardSize));
-      virBoard[gameOptions.treasure.location] = 't';
-    }while (gameOptions.treasure.location == 0)
+      gameOptions.treasure.location[0] = Math.floor(Math.random() * Math.sqrt(gameOptions.boardSize));
+      gameOptions.treasure.location[1] = Math.floor(Math.random() * Math.sqrt(gameOptions.boardSize));
+    } while (gameOptions.treasure.location[0] == 0 && gameOptions.treasure.location[1] == 0);
 
-    //RANDOMLY PLACE DRAGON NODE (LOCATION 0 AND DRAGON.LOCATOIN NOT ALLOWED)
     do {
-      gameOptions.dragon.location = Math.round(Math.floor(Math.random() * gameOptions.boardSize));
-      virBoard[gameOptions.dragon.location] = 'd';
-    } while (gameOptions.dragon.location == 0 || gameOptions.dragon.location == gameOptions.treasure.location)
+      gameOptions.dragon.location[0] = Math.floor(Math.random() * Math.sqrt(gameOptions.boardSize));
+      gameOptions.dragon.location[1] = Math.floor(Math.random() * Math.sqrt(gameOptions.boardSize));
+    } while (gameOptions.dragon.location[0] === 0 && gameOptions.dragon.location[1] === 0 || gameOptions.dragon.location[0] === gameOptions.treasure.location[0] && gameOptions.dragon.location[1] === gameOptions.treasure.location[1])
 
+    nodeLocation(gameOptions.treasure.location[0], gameOptions.treasure.location[1], 't');
+    nodeLocation(gameOptions.dragon.location[0], gameOptions.dragon.location[1], 'd');
   }
 }
-let virBoard = Array(Math.sqrt(gameOptions.boardSize)); //INITIALIZE VIRTUAL BOARD ARRAY
+let virBoard = Array(0); //INITIALIZE VIRTUAL BOARD ARRAY
 
+
+function moveChar(x,y) {
+  console.log(elCurLoc);
+  let curLocId = elCurLoc[0].toString() + elCurLoc[1].toString();
+
+  elCurLoc[0] += y;
+  elCurLoc[1] += x;
+
+  try {
+    let newLocId = elCurLoc[0].toString() + elCurLoc[1].toString();
+    document.getElementById(newLocId).classList.add('hero');
+    document.getElementById(curLocId).classList.remove('hero');
+    chkLocation();
+  }
+  catch(err) {
+    elCurLoc[0] -= y;
+    elCurLoc[1] -= x;
+
+    alert('You have run into the wall');
+
+    return;
+  }
+}
+
+function nodeLocation(x,y,gameNode) {
+  virBoard[y][x] = gameNode;
+}
 
 //CREATES VIRTUAL BOARD - DEFAULT 6 (sqrt of 36) ROWS OF 6 (sqrt of 36) CELLS
 for (let i=0; i < Math.sqrt(gameOptions.boardSize); i++) {
@@ -94,18 +131,11 @@ for (let i=0; i < Math.sqrt(gameOptions.boardSize); i++) {
 }
 
 //CREATE BOARD TILES
-for (let i=0; i < gameOptions.boardSize; i++) {
+for (let i=0; i < Math.sqrt(gameOptions.boardSize); i++) {
     gameBoard.createBoard(i);
-    console.log(virBoard[i]);
 }
 
-//CODE FOR INITIAL PLACEMENT OF HERO (TILE 0)
-let elHero = document.getElementById("0");
-elHero.classList.add("hero");
-
 gameBoard.fillBoard();
-console.log(gameOptions.dragon.location);
-console.log(gameOptions.treasure.location);
 
 for (let i=0; i < virBoard.length; i++) {
   console.log(virBoard[i]);
